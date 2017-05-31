cfd2e17a14bdccd8b3ee9e31c27680000a6d73a7
Matt Dewey <mattfromseattle@me.com>
2017-05-31 09:27:52 -0700

    Initial commit.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherforecastApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the weatherforecastApp
+ */
+angular.module('weatherforecastApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name weatherforecastApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the weatherforecastApp
+ */
+angular.module('weatherforecastApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
